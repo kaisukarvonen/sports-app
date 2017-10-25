4dabac28a5e001ecb11f6ddc92f0835cfc0f3def
Kaisu Karvonen <prosp33r@gmail.com>
2017-10-26 00:05:26 +0300

    separated backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,14 @@ import { fetchObjects } from './actions/objects';
 class App extends React.Component {
   componentWillMount() {
     console.log('here');
-    this.props.fetchObjects();
+    //this.props.fetchObjects();
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => console.log(users));
   }
 
   render() {
-    console.log('render:' + this.props.items);
+    //console.log(this.props.items);
     return (
       <div>
         <h2>Hello component, here we are</h2>
